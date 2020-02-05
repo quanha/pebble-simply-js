9cc58f08328dd0506938732549c41140514be8c0
Quan Ha <quanhh.me@gmail.com>
2020-02-05 12:05:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ simply.on('singleClick', function(e) {
   if (e.button === 'up') {
     navigator.geolocation.getCurrentPosition(function(pos) {
       var coords = pos.coords;
-      var weatherUrl = 'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22';
+      var weatherUrl = 'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22&lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
       ajax({ url: weatherUrl, type: 'json' }, function(data) {
-        simply.text({ title: coords.latitude, subtitle: coords.latitude });
+        simply.text({ title: coords.latitude, subtitle: coords.longitude });
       });
     });
   }
