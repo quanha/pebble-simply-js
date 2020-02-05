c09dfea8d5c6c42cae751bb4f7db760730d77697
Quan Ha <quanhh.me@gmail.com>
2020-02-05 16:00:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ simply.on('singleClick', function(e) {
   if (e.button === 'up') {
     navigator.geolocation.getCurrentPosition(function(pos) {
       var coords = pos.coords;
-      var weatherUrl = 'https://samples.openweathermap.org/data/2.5/weather?appid=b6907d289e10d714a6e88b30761fae22&lat=' + coords.latitude + '&lon=' + coords.longitude;
+      var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=243d56456673442f6e9ca2467363b70c&lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
       ajax({ url: weatherUrl, type: 'json' }, function(data) {
-        var temp = data.main.temp;
-        temp = (temp - 32) * 5 /9
         var body = 
             "Name:" + data.name + "\n" +
-            "Description: " + data.weather.description +  "\n" +
-            "Temperature: " + temp + "\n" +
+            "Description: " + data.weather[0].description +  "\n" +
+            "Temperature: " + data.main.temp + "(" + data.main.temp_min + " ~ " + data.main.temp_max + ")" + "\n" +
             "Humidity: " + data.main.humidity;
         Pebble.showSimpleNotificationOnPebble('Open Weather', body);
       });
