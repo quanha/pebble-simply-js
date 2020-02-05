1608e6973d2a23865c110bce1e7bf302421d0c3c
Quan Ha <quanhh.me@gmail.com>
2020-02-05 11:26:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var count = parseInt(localStorage.getItem('count')) || 0;
+console.log('Simply.js demo!');
 
 simply.on('singleClick', function(e) {
-  if (e.button === 'up') {
-    simply.subtitle(++count);
-  } else if (e.button === 'down') {
-    simply.subtitle(--count);
-  }
-  localStorage.setItem('count', count);
+  console.log(util2.format('single clicked $button!', e));
+  simply.subtitle('Pressed ' + e.button + '!');
 });
 
-simply.text({ title: 'Counter', subtitle: count });
+simply.on('longClick', function(e) {
+  console.log(util2.format('long clicked $button!', e));
+  simply.vibe();
+  simply.scrollable(e.button !== 'select');
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});
+
+simply.setText({
+  title: 'Simply Demo!',
+  body: 'This is a demo. Press buttons or tap the watch!',
+}, true);
